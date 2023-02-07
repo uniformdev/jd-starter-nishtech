ea572b17c8621177badce22bf218799f0c725130
Alex Shyba <alex.shyba@uniform.dev>
2023-02-06 20:05:27 -0800

    chore: revalidation logging

--- apps/baseline/src/pages/api/revalidate.ts
+++ apps/baseline/src/pages/api/revalidate.ts
@@ -24,6 +24,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   } catch (err) {
     // If there was an error, Next.js will continue
     // to show the last successfully generated page
+    console.error({ err });
     return res.status(500).send('Error revalidating!');
   }
 }
